b8c028768129e26427e4a1a5639aa8654d721f2d
svnv-svsv-jm <janmail1990@gmail.com>
2024-10-18 16:35:53 +0200

    reverse sync

--- src/svsvllm/app/ui/session_state.py
+++ src/svsvllm/app/ui/session_state.py
@@ -1,11 +1,15 @@
-# pylint: disable=no-member
-__all__ = ["SessionState"]
+# pylint: disable=no-member,unnecessary-dunder-call,global-variable-not-assigned
+__all__ = ["SessionState", "session_state"]
 
 import typing as ty
 from loguru import logger
 from pydantic import BaseModel, Field, SecretStr, field_validator
 import streamlit as st
-from streamlit.runtime.state.safe_session_state import SafeSessionState
+from streamlit.runtime.state import (
+    SafeSessionState,
+    SessionStateProxy,
+    SessionState as _SessionState,
+)
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from langchain_huggingface import ChatHuggingFace
@@ -18,98 +22,114 @@ from svsvllm.defaults import EMBEDDING_DEFAULT_MODEL, DEFAULT_LLM, OPENAI_DEFAUL
 from .callbacks import BaseCallback
 
 
+_locals: dict[str, ty.Any] = {"__avoid_recursive": False, "_bind": None}
+
+
+class PatchRecursive:
+    """Temporarily deactivate recursion."""
+
+    def __enter__(self) -> "PatchRecursive":
+        """Edit value."""
+        _locals["__avoid_recursive"] = True
+        return self
+
+    def __exit__(self, *args: ty.Any, **kwargs: ty.Any) -> None:
+        """Edit value."""
+        _locals["__avoid_recursive"] = False
+
+
 class SessionState(BaseModel):
     """Custom Streamlit session state, in order to revise all present keys, validate them, etc."""
 
     has_chat: bool = Field(
         default=True,
         description="Whether the app has a chatbot or not.",
-        validate_default=False,
+        validate_default=True,
     )
     language: ty.Literal["English", "Italian"] = Field(
         default="English",
         description="App language.",
-        validate_default=False,
+        validate_default=True,
     )
     new_language: ty.Literal["English", "Italian"] = Field(
         default="English",
         description="App language we are switching to.",
-        validate_default=False,
+        validate_default=True,
     )
-    openai_api_key: SecretStr = Field(
+    openai_api_key: SecretStr | None = Field(
         default=None,
         description="API key for OpenAI.",
-        validate_default=False,
+        validate_default=True,
     )
     openai_model_name: str = Field(
         default=OPENAI_DEFAULT_MODEL,
         description="OpenAI model name.",
-        validate_default=False,
+        validate_default=True,
     )
     model_name: str = Field(
         default=DEFAULT_LLM,
         description="HuggingFace model name.",
-        validate_default=False,
+        validate_default=True,
     )
     embedding_model_name: str = Field(
         default=EMBEDDING_DEFAULT_MODEL,
         description="RAG embedding model name.",
-        validate_default=False,
+        validate_default=True,
     )
-    uploaded_files: list[UploadedFile] = Field(
+    uploaded_files: list[UploadedFile] | None = Field(
         default=None,
         description="Uploaded files.",
-        validate_default=False,
+        validate_default=True,
     )
     callbacks: dict[str, BaseCallback] = Field(
         default={},
         description="Uploaded files.",
-        validate_default=False,
+        validate_default=True,
     )
     saved_filenames: list[str] = Field(
         default=[],
         description="Names of the uploaded files after written to disk.",
-        validate_default=False,
+        validate_default=True,
     )
     page: ty.Literal["main", "settings"] = Field(
         default="main",
         description="Page we are displaying.",
-        validate_default=False,
+        validate_default=True,
     )
-    openai_client: OpenAI = Field(
+    openai_client: OpenAI | None = Field(
         default=None,
         description="OpenAI client.",
-        validate_default=False,
+        validate_default=True,
     )
-    hf_model: AutoModelForCausalLM = Field(
+    hf_model: AutoModelForCausalLM | None = Field(
         default=None,
         description="HuggingFace model.",
-        validate_default=False,
+        validate_default=True,
     )
-    tokenizer: AutoTokenizer = Field(
+    tokenizer: AutoTokenizer | None = Field(
         default=None,
         description="HuggingFace tokenizer.",
-        validate_default=False,
+        validate_default=True,
     )
-    db: FAISS = Field(
+    db: FAISS | None = Field(
         default=None,
         description="Document database.",
-        validate_default=False,
+        validate_default=True,
     )
-    retriever: BaseRetriever = Field(
+    retriever: BaseRetriever | None = Field(
         default=None,
         description="Retriever object created from databse.",
-        validate_default=False,
+        validate_default=True,
     )
-    history_aware_retriever: RetrieverOutputLike = Field(
+    history_aware_retriever: RetrieverOutputLike | None = Field(
         default=None,
         description="History aware retriever.",
-        validate_default=False,
+        validate_default=True,
     )
-    chat_model: ChatHuggingFace = Field(
+    chat_model: ChatHuggingFace | None = Field(
         default=None,
         description="Open source HuggingFace model LangChain wrapper.",
-        validate_default=False,
+        validate_default=True,
     )
 
     class Config:
@@ -133,37 +153,104 @@ class SessionState(BaseModel):
 
     def __init__(self, *args: ty.Any, **kwargs: ty.Any) -> None:
         super().__init__(*args, **kwargs)
-
-        # Initialize bound state
-        self._bind: ty.MutableMapping | SafeSessionState | None = None
-
         # Immediately sync
         self.sync()
+        # Patch
+        self.patch(st.session_state)
+
+    @property
+    def _bind(self) -> SessionStateProxy | SafeSessionState | None:
+        """Access the bound sesstion state."""
+        r: SessionStateProxy | SafeSessionState | None = _locals["_bind"]
+        if r is not None:
+            assert isinstance(r, (_SessionState, SessionStateProxy, SafeSessionState))
+        return r
+
+    @_bind.setter
+    def _bind(self, value: SessionStateProxy | SafeSessionState) -> None:
+        """Set the bound object."""
+        _locals["_bind"] = value
+
+    @property
+    def __avoid_recursive(self) -> bool:
+        """Access the bound sesstion state."""
+        r: bool = _locals["__avoid_recursive"]
+        return r
+
+    @__avoid_recursive.setter
+    def __avoid_recursive(self, value: bool) -> None:
+        """Set the bound object."""
+        _locals["__avoid_recursive"] = value
 
     def sync(self) -> None:
         """Sync states."""
+        logger.debug(f"Syncing")
         for key, val in self.to_dict().items():
             logger.trace(f"Syncing: {key}")
             self.session_state[key] = val
 
-    def bind(self, session_state: ty.MutableMapping | SafeSessionState) -> None:
+    def bind(self, session_state: SessionStateProxy | SafeSessionState) -> None:
         """Bind this model to Streamlit's session state."""
         logger.trace(f"Binding to {type(session_state)}")
-        self._bind = session_state
+        self._bind = self.patch(session_state)
         self.sync()
 
     def unbind(self) -> None:
         """Unbind."""
+        logger.trace("Unbinding")
         self._bind = None
 
+    def patch(
+        self,
+        state: SessionStateProxy | SafeSessionState | None = None,
+    ) -> SessionStateProxy | SafeSessionState:
+        """We patch the original Streamlit state so that when that one is used, it automatically syncs with this class."""
+        if state is None:
+            state = self.session_state
+        logger.trace("Patching `__setitem__` and `__setattr__`")
+        # Patch
+        __setitem__original = state.__class__.__setitem__
+        __setattr__original = state.__class__.__setattr__
+
+        def patched_setitem(
+            obj: SessionStateProxy | SafeSessionState,
+            key: ty.Any,
+            value: ty.Any,
+        ) -> None:
+            # Call the original method
+            __setitem__original(obj, key, value)  # type: ignore
+            # Call custom function
+            with PatchRecursive():
+                self.__setitem__(key, value)
+
+        def patched_setattr(
+            obj: SessionStateProxy | SafeSessionState,
+            key: ty.Any,
+            value: ty.Any,
+        ) -> None:
+            # Call the original method
+            __setattr__original(obj, key, value)  # type: ignore
+            # Call custom function
+            with PatchRecursive():
+                self.__setattr__(key, value)
+
+        # Replace the original method with the patched one
+        state.__class__.__setitem__ = patched_setitem  # type: ignore
+        state.__class__.__setattr__ = patched_setattr  # type: ignore
+        logger.trace("Patched `__setitem__` and `__setattr__`")
+        return state
+
     @property
-    def session_state(self) -> ty.MutableMapping | SafeSessionState:
+    def session_state(self) -> SessionStateProxy | SafeSessionState:
         """Streamlit session state."""
         if self._bind is not None:
             logger.trace("Session state is bounded explicitly.")
-            return self._bind
-        logger.trace("Session state is not bounded explicitly")
-        return st.session_state
+            state = self._bind
+        else:
+            logger.trace("Session state is not bounded explicitly")
+            state = st.session_state
+        # Return
+        return state
 
     def __len__(self) -> int:
         """Number of user state and keyed widget values in session_state."""
@@ -171,33 +258,22 @@ class SessionState(BaseModel):
 
     def __getitem__(self, key: str) -> ty.Any:
         """Return the state or widget value with the given key."""
-        logger.trace(f"Getting `{key}`")
         return self.session_state.__getitem__(key)
 
     def __setitem__(self, key: str, value: ty.Any) -> None:
         """Set the value of the given key."""
-        logger.trace(f"Setting `{key}` with `{value}`")
-        try:
-            super().__setitem__(key, value)  # type: ignore
-        except:
-            logger.trace("Superclass has no `__setitem__()` method.")
-            pass
-        self.session_state[key] = value
+        self.__setattr__(key, value)
 
     def __setattr__(self, key: str, value: ty.Any) -> None:
         """Set the value of the given key."""
-        logger.trace(f"Setting `{key}` with `{value}`")
         super().__setattr__(key, value)  # type: ignore
-        self.session_state[key] = value
-
-    def __getattr__(self, key: str) -> ty.Any:
-        """Allow key access."""
-        logger.trace(f"Getting `{key}`")
-        try:
-            return self[key]
-        except KeyError as ex:
-            raise AttributeError("Invalid key.") from ex
+        if self.__avoid_recursive:
+            return
+        self.session_state.__setattr__(key, value)
 
     def to_dict(self) -> dict[str, ty.Any]:
         """Dump model."""
         return self.model_dump()
+
+
+session_state = SessionState()
--- tests/svsvllm/app/ui/test_session_state.py
+++ tests/svsvllm/app/ui/test_session_state.py
@@ -1,3 +1,4 @@
+# pylint: disable=unnecessary-dunder-call
 import pytest
 from unittest.mock import patch, MagicMock
 from loguru import logger
@@ -10,25 +11,17 @@ from pydantic_core import ValidationError
 from svsvllm.app.ui.session_state import SessionState
 
 
-# def test_session_state_is_singleton(apptest: AppTest) -> None:
-#     """Test there is only one instance."""
-#     assert SessionState() is SessionState()
-#     assert SessionState().session_state is SessionState().session_state
-#     state = SessionState()
-#     state.bind(apptest.session_state)
-#     assert SessionState().session_state is SessionState().session_state
-
-
 @pytest.mark.parametrize("bind", [False, True])
 @pytest.mark.parametrize(
-    "field, value, validation_fails",
+    "field, value, validation_fails, reverse_sync",
     [
-        ("has_chat", False, False),
-        ("has_chat", True, False),
-        ("has_chat", 3, True),
-        ("new_language", "English", False),
-        ("openai_model_name", "what", False),
-        ("page", "yo", True),
+        ("has_chat", False, False, False),
+        ("has_chat", True, False, False),
+        ("has_chat", 3, True, False),
+        ("new_language", "English", False, False),
+        ("openai_model_name", "what", False, False),
+        ("page", "yo", True, False),
+        ("openai_model_name", "what", False, True),
     ],
 )
 def test_session_states_are_synced(
@@ -37,29 +30,40 @@ def test_session_states_are_synced(
     field: str,
     value: ty.Any,
     validation_fails: bool,
+    reverse_sync: bool,
 ) -> None:
     """Test `SessionState` is always synced with `st.session_state`."""
     # Create state
-    state = SessionState()
-    logger.info(f"Creating session state: {type(state)}")
+    our_state = SessionState()
+    logger.info(f"Creating session state: {type(our_state)}")
 
     # Bind
     if bind:
-        state.bind(apptest.session_state)
-        ss = apptest.session_state
+        our_state.bind(apptest.session_state)
+        st_state = apptest.session_state
     else:
-        state.unbind()
-        ss = st.session_state  # type: ignore
+        our_state.unbind()
+        st_state = st.session_state  # type: ignore
 
     # Here we are passing an invalid value and we expect a ValidationError
     if validation_fails:
         with pytest.raises(ValidationError):
-            setattr(state, field, value)
+            setattr(our_state, field, value)
         return
 
-    # Mess around with 'field'
-    setattr(state, field, value)
-    assert ss[field] == getattr(state, field)
+    # Update field
+    if reverse_sync:
+        with patch.object(our_state, "__setattr__", side_effect=our_state.__setattr__) as mock:
+            st_state[field] = value
+            # Test our class method was called even if we set the state directly
+            mock.assert_called()
+    else:
+        setattr(our_state, field, value)
+
+    # Test they stay synced
+    assert getattr(st_state, field) == value
+    assert getattr(our_state, field) == value
+    assert our_state[field] == st_state[field]
 
 
 if __name__ == "__main__":
